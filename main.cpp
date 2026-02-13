a98e36735dea7ae270173ff070ebca2a3734d877
NT-Bean <samrahmani2010@gmail.com>
2026-02-13 14:07:58 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+const unsigned int width = 1600;
+const unsigned int height = 900;
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(width, height, "StarChart", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "woops the window didnt get make properly" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+
+    gladLoadGL();
+    glViewport(0, 0, width, height);
+
+
+    glEnable(GL_DEPTH_TEST);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    return 0;
+}
